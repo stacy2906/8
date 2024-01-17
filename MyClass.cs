54b511e744ac073454d43952ea8bf1aa2d4d78b7
stacy2906 <124590794+stacy2906@users.noreply.github.com>
2024-01-17 20:21:10 +0200

    Add files via upload

---
+++ MyClass.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp31
+{ /// <summary>
+
+  /// класс
+
+  /// </summary>
+    //internal class MyClass
+    //{
+    //    /// <summary>
+
+    //    /// открытое поле
+
+    //    /// </summary>
+
+    //    public static int number;
+
+    //    public static int price;
+
+    //    public const int a = 125;
+
+    //    /// <summary>
+
+    //    /// метод вывода
+
+    //    /// </summary>
+
+    //    public void Print()
+
+    //    {
+
+    //        Console.WriteLine(number);
+
+    //    }
+
+
+
+    //    public void ID()
+
+    //    {
+
+    //        Console.WriteLine(price);
+
+    //    }
+
+
+
+    //    public void ID2()
+
+    //    {
+
+    //        Console.WriteLine(a);
+
+    //    }
+    //    static MyClass()
+
+    //    {
+
+    //        Console.Write("Статический конструкор");
+
+    //        number = 1;
+
+    //    }
+
+
+
+    //    public MyClass()
+
+    //    {
+
+    //        Console.WriteLine("констр конструктор");
+
+    //        number = 2;
+
+    //    }
+    //}  
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using ConsoleApp31;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //MyClass myClass = new MyClass();
+
+        //MyClass instance1 = new MyClass();
+
+        //MyClass instance2 = new MyClass();
+
+        //MyClass instance3 = new MyClass();
+
+        //MyClass instance4 = new MyClass();
+
+        //MyClass.number = 10;
+
+        //MyClass.number = 20;
+
+        //MyClass.price = 30;
+
+
+
+        //myClass.Print();
+
+        //instance1.Print();
+
+        //instance2.Print();
+
+        //instance3.ID();
+
+        //instance4.ID2();
+
+        //Console.ReadKey();
+
+
+
+        //Задача 2.
+        //Point point1 = new Point(1, 1);
+        //Point point2 = new Point(4, 5);
+        //double d= Point.LengthPoints(point1, point2);
+        //Console.WriteLine("расстояния между двумя точками " + d);
+
+
+        //Задача 1
+        Point point1 = new Point(3.0, 4.0);
+        Point point2 = new Point();
+        Point point3 = new Point(-2.5, 7.8);
+
+        point1.Print();
+        point2.Print();
+        point3.Print();
+
+        Console.WriteLine("Общее количество созданных экземпляров Point: " + Point.GetInstanceCount());
+
+    }
+}
